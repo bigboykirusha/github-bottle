bd2a8fffe1584a14eb93e6b24993cbf59c151cff
bigboykirusha <105214100+bigboykirusha@users.noreply.github.com>
2023-06-02 03:17:06 +0300

    gh-pages version

--- src/Utils/fetchRepos.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchRepos = async (language: string, page: number) => {
-  return await axios
-    .get(
-      `https://api.github.com/search/repositories?q=stars:>5000+language:${language}&sort=stars&order=desc&per_page=8&page=${page}`
-    )
-    .then((resp) => resp.data);
-};
